dbef0e41b9c9e5971c1815334209959fb6b93b21
KingrogKDR <abhisheksaikia766@gmail.com>
2025-04-09 19:27:51 +0530

    project setup

---
+++ rablo-official/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" py-2 px-2 block gap-2">
+      <button className="nav-button1">Hello1</button>
+      <button className="nav-button2">Hello2</button>
+    </div>
+  );
+}
