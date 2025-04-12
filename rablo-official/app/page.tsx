6421958ef2496808d6299a34d73128d539d1ad38
Harsh <harshg889691@gmail.com>
2025-04-12 18:57:52 +0530

    Header Component Done -Harsh Gupta

--- rablo-official/app/page.tsx
+++ rablo-official/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import Header from "./components/Header/Header";
+import Section from "./components/Section/Section";
 
 export default function Home() {
   return (
-    <div className=" py-2 px-2 block gap-2">
-      <button className="nav-button1">Hello1</button>
-      <button className="nav-button2">Hello2</button>
+    // <div className=" py-2 px-2 block gap-2">
+    //   <button className="nav-button1">Hello1</button>
+    //   <button className="nav-button2">Hello2</button>
+    // </div>
+    <div>
+      <Header/>
+      <Section/>
     </div>
   );
 }
