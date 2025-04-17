5512893bfb01dfae779d79161aca597c476162b7
Harsh <harshg889691@gmail.com>
2025-04-17 21:24:55 +0530

    Header And HeroSection Done

--- rablo-official/app/components/MainSection/MainSection.tsx
+++ rablo-official/app/components/MainSection/MainSection.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "./HeroSection/HeroSection";
 
 const MainSection = () => {
   return (
-    <div className='w-full h-[100vh] section-container bg-[url(/header/images/backgroundImg.png)] bg-center bg-cover bg-no-repeat'>
+    <main>
+      <HeroSection />
+    </main>
+  );
+};
 
-    </div>
-  )
-}
-
-export default MainSection
+export default MainSection;
--- rablo-official/app/page.tsx
+++ rablo-official/app/page.tsx
@@ -1,9 +1,5 @@
 import MainSection from "./components/MainSection/MainSection";
 
 export default function Home() {
-  return (
-    <main>
-      <MainSection/>
-    </main>
-  );
+  return <MainSection />;
 }
