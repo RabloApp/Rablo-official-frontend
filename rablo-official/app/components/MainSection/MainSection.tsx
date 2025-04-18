dfaa62aa95d065c38c2ef0b02a39fe22235ae203
KingrogKDR <abhisheksaikia766@gmail.com>
2025-04-18 20:07:11 +0530

    explore section

--- rablo-official/app/components/MainSection/MainSection.tsx
+++ rablo-official/app/components/MainSection/MainSection.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./HeroSection/HeroSection";
+import ExploreTutors from "./ExploreTutors/ExploreTutors";
 
 const MainSection = () => {
   return (
     <main>
       <HeroSection />
+      <ExploreTutors />
     </main>
   );
 };
