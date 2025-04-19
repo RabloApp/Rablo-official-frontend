4b83bf26b2a713917f209bd83320dca1ddf6238a
KingrogKDR <abhisheksaikia766@gmail.com>
2025-04-20 00:17:59 +0530

    crafted for tutors section and button ui changes

---
+++ rablo-official/StaticData/craftedForTutors.ts
@@ -0,0 +1,32 @@
+interface Feature {
+  image: string;
+  title: string;
+  description: string;
+}
+
+export const features: Feature[] = [
+  {
+    image: "/craftedForTutors/image1.svg",
+    title: "Unlimited student connections",
+    description:
+      "No limits, no restrictions. Connect with as many students as you want, manage multiple batches, and grow your community without worrying about caps or extra charges. Whether you’re teaching five or five hundred, Rablo scales with you.",
+  },
+  {
+    image: "/craftedForTutors/image2.svg",
+    title: "Payment protection & Auto-credit",
+    description:
+      "Focus on teaching while we handle the money matters. Rablo ensures your payments are secure, on time, and automatically credited to your account after every class or course. No chasing fees, no payment delays — just hassle-free earnings.",
+  },
+  {
+    image: "/craftedForTutors/image3.svg",
+    title: "Create test & progress report",
+    description:
+      "Design and share tests effortlessly. Assess student understanding with customizable tests tailored to your teaching goals. Track performance with clarity. Get detailed reports that show student improvement and areas to focus on.",
+  },
+  {
+    image: "/craftedForTutors/image4.svg",
+    title: "Live classes & recording",
+    description:
+      "Host live classes in real-time, engage directly with your students, and record every session for easy access later. Students can revisit lessons anytime, reinforcing key concepts. Track progress, ensure no one falls behind, and provide an interactive learning experience.",
+  },
+];
--- rablo-official/components/MainSection/MainSection.tsx
+++ rablo-official/components/MainSection/MainSection.tsx
@@ -7,11 +7,13 @@ import Faq from "./FAQ/Faq";
 import JoinOption from "./JoinOption/JoinOption";
 import Benefits from "./Benefits/Benefits";
 import Testimonials from "./Testimonials/Testimonials";
+import CraftedForTutors from "./CraftedForTutors/CraftedForTutors";
 
 const MainSection = () => {
   return (
     <main>
       <HeroSection />
+      <CraftedForTutors />
       <WorkSection/>
       <Benefits/>
       <ExploreTutors />
