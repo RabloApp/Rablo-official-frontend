b165cb8b308318895c3c777f4fb0b6af986f62b7
Adarsh Kumar <adarshkumar42763@gmail.com>
2025-04-19 19:44:48 +0530

    WorkDone

--- rablo-official/app/components/MainSection/WorkSection/WorkSection.tsx
+++ rablo-official/app/components/MainSection/WorkSection/WorkSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WorkSection = () => {
   return (
-    <div className="w-[87.86vw] mx-auto relative mb-4" >
+    <div className="w-[87.86vw]  mx-auto relative mb-4" >
         <img src="/Background Shapes.png" alt="bgImage" />
       <div className="font-bold text-center text-3xl p-3 absolute top-[3rem] left-[35vw]">
         How does it work ?
@@ -17,13 +17,13 @@ const WorkSection = () => {
         </div>
       
       <div className="flex w-fit mx-auto gap-8 mt-4 pb-12">
-        <div className="w-[28vw] h-[22vh] rounded-xl mt-5 bg-[#D2DAFF]">
+        <div className="w-[30vw] h-[22vh] rounded-xl mt-5 bg-[#D2DAFF]">
           <img src="/Frame 50.png" className="h-[7vh] mx-auto mt-3" alt="frame" />
           <p className="w-fit mx-auto font-bold mt-3 text-xl">Connect</p>
           <p className="w-fit mx-auto  mt-2 ">Chat or call to discuss availability and goals.</p>
         </div>
         <div className="flex relative">
-        <div className="w-[28vw] h-[22vh] rounded-xl mt-5 bg-[#D2DAFF]">
+        <div className="w-[30vw] h-[22vh] rounded-xl mt-5 bg-[#D2DAFF]">
           <img src="/Frame 50 (1).png" className="h-[7vh] mx-auto mt-3" alt="fame2" />
           <p className="w-fit mx-auto font-bold mt-3 text-xl">Learn</p>
           <p className="w-fit mx-auto  mt-2 ">Start lessons and track your progress overtime.</p>
