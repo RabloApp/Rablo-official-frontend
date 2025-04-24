2e8d5c7fe0a6e1ec9becd65a2de5d2ce2641d05d
Harsh <harshg889691@gmail.com>
2025-04-24 21:26:06 +0530

    sidebar complete

--- rablo-official/components/Contact-us/Sidebar/Sidebar.tsx
+++ rablo-official/components/Contact-us/Sidebar/Sidebar.tsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import { sidebarData, sidebarIcons } from "@/StaticData/Sidebar";
+import Image from "next/image";
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="w-[27.34vw] h-[56.25vw] bg-[#0A66C2] p-[2.08vw] flex flex-col gap-[4.17vw] relative">
+      <div className="w-[14.53vw] flex gap-[1.3vw]">
+        <Image
+          src="/sidebar/images/arrow.svg"
+          className="w-[1.11vw] h-[1.11vw]"
+          width={0}
+          height={0}
+          alt=""
+        />
+        <Image
+          src="/sidebar/images/logo.svg"
+          className="w-[7.34vw] h-[1.67vw]"
+          width={0}
+          height={0}
+          alt=""
+        />
+      </div>
 
-export default Sidebar
+      <div className="absolute right-0">
+        <Image
+          src="/sidebar/images/Ellipse.svg"
+          className="w-[2.08vw] h-[4.17vw]"
+          width={0}
+          height={0}
+          alt=""
+        />
+      </div>
+
+      <div className="w-[23.18vw] h-[5.83vw] flex flex-col gap-[0.83vw] text-[#FFFFFF]">
+        <h1 className="font-[Poppins] font-semibold text-[1.67vw] ">
+          Get in touch
+        </h1>
+        <p className="font-normal text-[1vw]">
+          We’d love to hear from you. Our friendly team is here to chat.
+        </p>
+      </div>
+
+      <div className="w-[23.18vw] h-[16.04] flex flex-col gap-[2.08vw] ">
+        {sidebarData.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="w-[23.18] flex justify-center items-center gap-[1vw] text-[#FFFFFF]"
+            >
+              <Image
+                src={item.image}
+                className="w-[1.67vw] h-[1.67vw]"
+                width={0}
+                height={0}
+                alt=""
+              />
+              <div className=" w-[20.68vw] h-[3.54vw] flex flex-col gap-[0.42vw] ">
+                <h1 className="font-[poppins] font-normal text-[1.25vw]">
+                  {item.title}
+                </h1>
+                <p className="font-[poppins] font-medium text-[0.95vw]">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="w-[13.33vw] flex flex-row gap-[1.67vw] pt-[1.56vw]">
+        {sidebarIcons.map((item, index) => {
+          return (
+            <Image
+              src={item.icon}
+              key={index}
+              className="w-[2.08vw] h-[2.08vw]"
+              width={0}
+              height={0}
+              alt=""
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
